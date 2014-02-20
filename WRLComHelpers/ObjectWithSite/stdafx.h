3e1056771822bed76d04916b60cc22c11aeda3f1
GabeDeBacker <g_debacker@hotmail.com>
2014-02-20 15:00:38 -0800

    Check In Solution

---
+++ WRLComHelpers/ObjectWithSite/stdafx.h
@@ -0,0 +1,17 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+// Headers for CppUnitTest
+#include "CppUnitTest.h"
+#include <OCIdl.h>
+#include <ShObjIdl.h>
+#include <wrl.h>
+#include <vector>
+
+// TODO: reference additional headers your program requires here
